de8b798ef2053fd19fd083356c693d162cfc051b
Elizabeth Lambos <elizabeth.lambos@gmail.com>
2014-01-22 11:44:05 -0500

    Started process of parse and FB login
    started to build a FB login for parse and added the link for the parse
    CDN to the header of each page

---
+++ js/main_script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+var user = new Parse.User();
+user.set("username", "liz nambos");
+user.set("password", "nass");
+user.set("email", "niz@example.com");
+  
+  
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
+
+
+$("#fb_login_button").click(function(){
+	fb_login();
+});
+
+
+
+/* what user data looks like
+
+{ "session_token": "wnc5c6b44m7n2fk74ul4hujuk", 
+"auth_data": { 
+	"facebook": { 
+		"id": "605697", 
+		"access_token": "CAADnyIkAxgUBAFBK6p9y56VcC7ZBI7z47hPjvXMZBEEkFRhSTyC9GQBETuApRO86Mh7B42OoujNPadcVeHJg39rn5sF7rZBT5zth120ij7Egxxz7ZB9kCw77EdPzKyDiBs4AyM9feUO3pmRhFcJLvZCL65Qcq35hPOIyR2RZA0ofUIBV0ae5iGrjrPShYcrIbBRMJEiXTYbKbDk1jRQ7h2yAODOas7Bb4ZD", 
+		"expiration_date": "2014-03-21T21:01:32.915Z" 
+		} 
+	}, 
+"id": "r9dVzEko4H", 
+"data": { 
+	"username": "yklsuokr249gaysqkgo4rqozg" }, 
+	"created_at": "2014-01-20T21:01:35Z", 
+	"updated_at": "2014-01-20T21:01:35Z" }*/
+
+
+
+
+
+
+});
