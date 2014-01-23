4288a9016f87d0058728a680214716b374fa9a78
Elizabeth Lambos <elizabeth.lambos@gmail.com>
2014-01-22 19:39:01 -0500

    All things should be back to normal Parse + FB loose progress

--- js/main_script.js
+++ js/main_script.js
@@ -1,27 +1,39 @@
 $(document).ready(function(){
 
-//creating new user, not sure how to do this w/ FB login	
+ Parse.initialize("x03F3RJiRYdtYPfeS7AHNOEDHL0cx2nzzJ4ztDOX", "mYTgTArAtPa24wEcsXfUQYT6NQmI0iG5iR6xHHDL");   
 
-var user = new Parse.User();
-user.set("username", "liz nambos");
-user.set("password", "nass");
-user.set("email", "niz@example.com");
-  
-  
-user.signUp(null, {
-  success: function(user) {
-    // Hooray! Let them use the app now.
-  },
-  error: function(user, error) {
-    // Show the error message somewhere and let the user try again.
-    alert("Error: " + error.code + " " + error.message);
+//translate FBlogin generated username into a real username
+
   }
-});
 
-//not sure how sheets read scripts written on html doc
-$("#fb_login_button").click(function(){
-	fb_login();
-});
+
+  function getUserInfo() {
+        FB.api('/me', function(response) {
+          var name = response.name;
+          var id = response.id;
+          //document.getElementById("status").innerHTML=str;
+            console.log(name);
+  
+
+      
+    }
+
+
+
+    /*function getPhoto()
+    {
+      FB.api('/me/picture?type=normal', function(response) {
+ 
+          var pic ="<img src='"+response.data.url+"'/>";
+          document.getElementById("user_FB_pic").innerHTML+=pic;
+ 
+    });
+
+
+      });*/
+
+/*not sure how sheets read scripts written on html doc
+*/
 
 
 
@@ -29,17 +41,17 @@ $("#fb_login_button").click(function(){
 
 { "session_token": "wnc5c6b44m7n2fk74ul4hujuk", 
 "auth_data": { 
-	"facebook": { 
-		"id": "605697", 
-		"access_token": "CAADnyIkAxgUBAFBK6p9y56VcC7ZBI7z47hPjvXMZBEEkFRhSTyC9GQBETuApRO86Mh7B42OoujNPadcVeHJg39rn5sF7rZBT5zth120ij7Egxxz7ZB9kCw77EdPzKyDiBs4AyM9feUO3pmRhFcJLvZCL65Qcq35hPOIyR2RZA0ofUIBV0ae5iGrjrPShYcrIbBRMJEiXTYbKbDk1jRQ7h2yAODOas7Bb4ZD", 
-		"expiration_date": "2014-03-21T21:01:32.915Z" 
-		} 
-	}, 
+  "facebook": { 
+    "id": "605697", 
+    "access_token": "CAADnyIkAxgUBAFBK6p9y56VcC7ZBI7z47hPjvXMZBEEkFRhSTyC9GQBETuApRO86Mh7B42OoujNPadcVeHJg39rn5sF7rZBT5zth120ij7Egxxz7ZB9kCw77EdPzKyDiBs4AyM9feUO3pmRhFcJLvZCL65Qcq35hPOIyR2RZA0ofUIBV0ae5iGrjrPShYcrIbBRMJEiXTYbKbDk1jRQ7h2yAODOas7Bb4ZD", 
+    "expiration_date": "2014-03-21T21:01:32.915Z" 
+    } 
+  }, 
 "id": "r9dVzEko4H", 
 "data": { 
-	"username": "yklsuokr249gaysqkgo4rqozg" }, 
-	"created_at": "2014-01-20T21:01:35Z", 
-	"updated_at": "2014-01-20T21:01:35Z" }*/
+  "username": "yklsuokr249gaysqkgo4rqozg" }, 
+  "created_at": "2014-01-20T21:01:35Z", 
+  "updated_at": "2014-01-20T21:01:35Z" }*/
 
 
 //create new query 
