cf6359557a74d916ca207527a5a9e36f5b395b5c
Elizabeth Lambos <elizabeth.lambos@gmail.com>
2014-02-02 10:02:51 -0500

    Got Friends pictures and names to display on invite friends page!!!!

--- invite_friends_page/js/script.js
+++ invite_friends_page/js/script.js
@@ -4,71 +4,51 @@ $(document).ready(function(){
 
 YUI().use('node', function (Y) {
 
+	friendListColumn1 = Y.one("#fb-column1");
+
   //INITIALIZE PARSE  
 
   Parse.$ = jQuery;
 
   Parse.initialize("x03F3RJiRYdtYPfeS7AHNOEDHL0cx2nzzJ4ztDOX",
-   "mYTgTArAtPa24wEcsXfUQYT6NQmI0iG5iR6xHHDL");  
+  	"mYTgTArAtPa24wEcsXfUQYT6NQmI0iG5iR6xHHDL");  
 
 
-user = Parse.User.current();
+  user = Parse.User.current();
+  console.log(user);
 
-function populateFriendList () {
+  function populateFriendList () {
 
-	FB.api('/me/friends', { fields : 'id', offset : 0, limit : 25 }, function(response) { 
+  	FB.api('/me/friends', { fields : 'id, name, picture, email'
+  		}, function(response) { 
+  		var FBArray = response.data;
+  		console.log(FBArray);	
+  		var numFriends = FBArray.length;
+  		console.log(numFriends);
 
-	 var outstring = '<p>Friends:</p>';
-       for (var i=0, l=response.data.length; i<l; i++) {
-         var friend = response.data[i];
-         outstring = outstring + '<fb:profile-pic uid="' + friend.id + '"  width="50" height="50" ></fb:profile-pic>' 
-		                        + '<fb:name uid="' + friend.id + '"></fb:name>&nbsp;&nbsp;&nbsp;'
-       }
-       document.getElementById('myFriendList').innerHTML = outstring; 
-       FB.XFBML.parse(document.getElementById('myFriendList'));
+  		for (var i=0, l=response.data.length; i<l; i++) {
+  			var friend = response.data[i];
+  			var friendID = friend.id;
+  			var fbFriendName = friend.name;
+  			console.log(fbFriendName);
+  			var friendPicLink = friend.picture.data.url;
 
-   };
+  			var content = Y.Lang.sub
+  			(Y.one('#fb_friends_invite_list').getHTML(), {
+  				id: friendID,
+  				friendName: fbFriendName,
+  				friendPicURL: friendPicLink
+  			
+  		});
+  			friendListColumn1.prepend(content);
 
-   Y.Array.each(response, function(val, i, arr) {
-  	friendName = val.get('asker');
-  	console.log(asker);
-  	askerID = asker.id;
-  	console.log(askerID);
-  	askerName = val.get('askerName');
-  	console.log(askerName);
+ }//for
 
-  //display the karma points balance on the two scoreboard areas (screen and mobile)  
+});//fb api
 
-  $(".row.scoreboard .scoreboard .karma_points_display")
-  .html("<span class='badge'>"+karmaPointsBalance+"</span>");
+  }//populate friend list
 
-  $(".top-navbar-icon.karma-points .badge")
-  .html("<span class='badge'>"+karmaPointsBalance+"</span>");
-
-    //load picture of query asker
-
-    function getAskerPic() {
-    	query = new Parse.Query(User);
-    	query.get(askerID, {
-    		success: function(item) {
-    			askerPic = item.get('userPic');
-    			console.log(askerPic);
-
-    			var content = Y.Lang.sub(Y.one('#friends_queries_section').getHTML(), {
-    				queryText: val.get('text'),
-    				timeStamp: val.get('timeStamp'),
-    				askerName: val.get('askerName'),
-    				id: val.id,
-    				privacylevel: val.get('privacylevel'),
-    				askerPicURL: askerPic,
-    				karmaPointsBal: karmaPointsBalance
-
-    			});
-
-    			contentColumn.prepend(content);
-   
-
-	
+populateFriendList();
 
 });//node
 
