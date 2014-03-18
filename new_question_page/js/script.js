4a42fe1364e3e8662713f1ede3566ee52f1cfb41
Elizabeth Lambos <elizabeth.lambos@gmail.com>
2014-03-18 16:52:57 -0400

    made some more formatting changes but will make look more like secret

--- new_question_page/js/script.js
+++ new_question_page/js/script.js
@@ -218,17 +218,17 @@ url7 = event.target.result;
      //console.log(url6);
 
      if ($(window).width() > 768) {
-      $("#query_area").css({"color":"#FFFFFF","text-shadow": "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black","font-family":"courier","font-size":"1.25em","width":"320px","height":"320px","border-radius":"0px","background-image":"url("+url7+")","background-size":"320px 320px"}); 
-      $("#pic_button").addClass("active");
-      $(".query_instructions:first").html("Query preview:")
+      $("#query_area").css({"color":"#FFFFFF","text-shadow": "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black","font-family":"courier","font-size":"1.25em","width":"320px","height":"320px","padding-top":"40px","border-radius":"0px","background-image":"url("+url7+")","background-size":"320px 320px"}); 
     }
 
      else {
-      $("#query_area").css({"color":"#FFFFFF","text-shadow": "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black","font-family":"courier","font-size":"1.25em","width":"240px","height":"240px","border-radius":"0px","background-image":"url("+url7+")","background-size":"320px 320px"}); 
-      $("#pic_button").addClass("active");
-      $(".query_instructions:first").html("Query preview:")
+      $("#query_area").css({"color":"#FFFFFF","text-shadow": "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black","font-family":"courier","font-size":"1.25em","width":"240px","height":"240px","padding-top":"30px","border-radius":"0px","background-image":"url("+url7+")","background-size":"320px 320px"}); 
+ 
     }
 
+      $("#pic_button").addClass("active");
+      $(".query_instructions:first").html("Query preview:")
+      $("span.help-block").addClass("hidden");
 
   }
 
